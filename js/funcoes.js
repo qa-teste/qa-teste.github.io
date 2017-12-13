fa6282d514288f5b14ff4d79268a9525c2d35fbb
qa-teste <33729256+qa-teste@users.noreply.github.com>
2017-12-13 19:38:53 -0200

    URL

--- js/funcoes.js
+++ js/funcoes.js
@@ -132,7 +132,8 @@ function calcularFmea(){
 }
 
 function gerarUrl(){
-	var url="file:///C:/Users/a0071351/Desktop/fmea/index.html";
+	//var url="file:///C:/Users/a0071351/Desktop/fmea/index.html";
+	var url="index.html";
 	var ocorrencia= $('#ocorrencia').val();
 	var severidade= $('#severidade').val();
 	var deteccao= $('#deteccao').val();
